dbf038c816285d9a6c6aff20537c260837a5f87d
Bruno Leonardo Michels <brunolm7@gmail.com>
2022-08-29 23:25:51 -0300

    Include client and server .ts files

---
+++ src/template/server.ts
@@ -0,0 +1,10 @@
+import Nullstack, { NullstackServerContext } from 'nullstack';
+import Application from './src/Application';
+
+const context = Nullstack.start(Application) as NullstackServerContext;
+
+context.start = async function start() {
+  // {{i18n_startupDoc}}
+}
+
+export default context;
