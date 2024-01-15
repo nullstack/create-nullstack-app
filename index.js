6f420520e939c4616fd6f6fc3ce9bfa3b74a6bdb
Andre Carvalho <andreluiz.carvalho@inatel.br>
2024-01-15 10:30:18 -0300

    Update CLI to be more interactive.

--- index.js
+++ index.js
@@ -1,22 +1,57 @@
 #! /usr/bin/env node
 
 const { Nulla, i18n } = require('./src/modules');
+const inquirer = require('inquirer');
+const { program } = require('commander');
+const { version } = require('./package.json');
 
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+program
+  .name('create-nullstack-app')
+  .version(version, '-v, --version', version)
+  .option('-t, --template', "Select a template to use", false);
 
-let args = process.argv.slice(2);
-const isTS = Nulla.isTS(args);
-const isTailwind = Nulla.isTailwind(args);
+program.parse();
 
-const tryNameRun = (name) => Nulla.tryRun(rl, name, isTS, isTailwind);
-let argName = args.join(' ');
+const { template } = program.opts();
+const projectName = program.args.map(n => n.trim()).filter(n => n).join('_');
 
-if (!argName) {
-  rl.question(i18n.questionName, tryNameRun);
-} else {
-  tryNameRun(argName);
+if (!projectName) {
+  console.log(`${i18n.error.unvalidName}\n`);
+  program.help();
 }
+
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'template',
+    message: i18n.template.questionName,
+    choices: [
+      i18n.template.blank,
+      i18n.template.blankTs,
+      i18n.template.tawild,
+      i18n.template.tawildTs,
+    ],
+    when: () => template
+  },
+])
+  .then((answers) => {
+    let isTS = false;
+    let isTailwind = false;
+
+    switch (answers.template) {
+      case i18n.template.blankTs:
+        isTS = true;
+        break;
+
+      case i18n.template.tawild:
+        isTailwind = true;
+        break;
+
+      case i18n.template.tawildTs:
+        isTS = true;
+        isTailwind = true;
+        break;
+    }
+
+    Nulla.tryRun(projectName, isTS, isTailwind);
+  });
